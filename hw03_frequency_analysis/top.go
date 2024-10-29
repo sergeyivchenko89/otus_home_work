a762739572d7bd658c92461f07fce7161c665f77
Сергей Ивченко <sergey.ivchenko89@yandex.ru>
2024-10-29 22:43:03 +0300

    Начало решения задачи поиска наиболее встречаемых слов в тексте

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,34 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"regexp"
+)
+
+var validID = regexp.MustCompile(`[^ \n\t]+`)
+
+func Top10(str string) []string {
+
+	frequencyDict := make(map[string]uint8)
+	resultDict := make(map[uint8]map[string]struct{})
+	result := make([]string, 0, 10)
+	var maxFrequency uint8 = 0
+
+	i := 0
+	for {
+		indices := validID.FindStringIndex(str[i:])
+		if indices == nil {
+			break
+		}
+		substring := validID.FindString(str[i:])
+		frequencyDict[substring]++
+		if frequencyDict[substring] > maxFrequency {
+			maxFrequency = frequencyDict[substring]
+		}
+		i += indices[1]
+	}
+
+	fmt.Println(frequencyDict)
+
+	return result
 }
